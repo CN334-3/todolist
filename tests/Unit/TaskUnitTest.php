89f6d92f8ddf4677e3c539becade90c685459f63
6210743032 <6210743032@student.tu.ac.th>
2022-04-20 21:45:13 +0700

    taskunittest8

--- tests/Unit/TaskUnitTest.php
+++ tests/Unit/TaskUnitTest.php
@@ -5,61 +5,24 @@ namespace Tests\Unit;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\Models\Task;
-//use Tests\Unit\Withfaker;
-//use Illuminate\Foundation\Testing\WithFaker;
-
 
 class TaskUnitTest extends TestCase
 { 
-/*
-    use RefreshDatabase, Withfaker;
 
-    public function test_model_user_exists()
+    public function testTaskCreation()
     {
-        $user = User::factory()->create();
 
-        $this->assertModelExists($user);
-    }
-*/
-
-    public function test_schema_support_integer()
-    {
-        $this->assertTrue(true);
     }
 
-    public function test_schema_detect_sql_injection()
+    //Test Schema that description is correct
+    public function test_schema_description()
     {
-        $this->assertTrue(true);
+
     }
 
-    public function test_schema_support_length_string()
+    //Test Schema that User id is correct
+    public function test_schema_userID()
     {
-        $this->assertTrue(true);
-    }
 
-    public function test_id_support_integer()
-    {
-        $this->assertTrue(true);
-    }
-
-    public function test_id_detect_null()
-    {
-        $task = new Task([
-            'user_id' => 1,
-            'description' => null,
-        ]);
-        $this->assertEquals(null, $task->description);
-    }
-
-    public function test_route_before_login_exists()
-    {
-        $welcomePage = $this->get('/');
-        $welcomePage->assertStatus(200);
-
-        $loginPage = $this->get('/login');
-        $loginPage->assertStatus(200);
-
-        $registerPage = $this->get('/register');
-        $registerPage->assertStatus(200);
     }
 }
